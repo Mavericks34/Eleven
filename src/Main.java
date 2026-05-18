36b9cffd66579a242775830ba32a5fdd5d3a3b0b
origin <httpsgit@github.com:Mavericks34/helloworldd.git>
2026-05-18 18:26:04 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static void determineTheYear (int year) {
+        if (year > 1584) {
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println(year + " Год является високосным");
+            } else {
+                System.out.println(year + " Год не является високосным");
+            }
         }
     }
+
+}
+
+
+
